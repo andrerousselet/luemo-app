50693f8a67eed2ef94bcc60217dc16e34281cc9e
Andre Rousselet <andre098@hotmail.com>
2022-08-19 19:59:57 -0300

    feat: add CRUD functions to firebase as example

--- src/firebase.js
+++ src/firebase.js
@@ -17,3 +17,4 @@ const auth = getAuth(app);
 // const analytics = getAnalytics(app);
 
 export { db, auth };
+// const analytics = getAnalytics(app);
---
+++ src/services/getClients.js
@@ -0,0 +1,123 @@
+import { useState } from 'react';
+import {
+  collection, doc, getDocs, addDoc, updateDoc, Timestamp, deleteDoc,
+} from 'firebase/firestore';
+import { db } from '../firebase';
+
+export default function Clients() {
+  const [clients, setClients] = useState([]);
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [id, setId] = useState('');
+  const [isUpdating, setIsUpdating] = useState(false);
+  const clientsCollectionRef = collection(db, 'clientes');
+
+  const getClients = async () => {
+    const data = await getDocs(clientsCollectionRef);
+    setClients(data.docs.map((document) => ({ ...document.data(), id: document.id })));
+  };
+
+  const createClient = async () => {
+    await addDoc(
+      clientsCollectionRef,
+      { name, surname, createdAt: Timestamp.now() },
+    );
+    setName('');
+    setSurname('');
+    setId('');
+    setIsUpdating(false);
+  };
+
+  const loadClientInfo = (client) => {
+    setIsUpdating(true);
+    setName(client.name);
+    setSurname(client.surname);
+    setId(client.id);
+    console.log(client.address);
+  };
+
+  const updateClient = async () => {
+    const clientDoc = doc(db, 'clientes', id);
+    const changes = { name, surname };
+    await updateDoc(clientDoc, changes);
+    setName('');
+    setSurname('');
+    setId('');
+    setIsUpdating(false);
+  };
+
+  const deleteClient = async (clientId) => {
+    const clientDoc = doc(db, 'clientes', clientId);
+    await deleteDoc(clientDoc);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center mb-4">
+        <input
+          className="mb-2"
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={({ target }) => setName(target.value)}
+        />
+        <input
+          className="mb-2"
+          type="text"
+          placeholder="Sobrenome"
+          value={surname}
+          onChange={({ target }) => setSurname(target.value)}
+        />
+        <div>
+          <button
+            className={isUpdating ? 'hidden' : 'border rounded p-2 bg-slate-200'}
+            type="button"
+            onClick={createClient}
+          >
+            CREATE CLIENT
+          </button>
+          <button
+            className={!isUpdating ? 'hidden' : 'border rounded p-2 bg-slate-200'}
+            type="button"
+            onClick={updateClient}
+          >
+            EDIT CLIENT
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <ul>
+          {clients
+            && clients
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map((client) => (
+                <li key={client.id}>
+                  {`${client.name} ${client.surname}`}
+                  <button
+                    className="border rounded p-1 bg-slate-100"
+                    type="button"
+                    onClick={() => loadClientInfo(client)}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    className="border rounded p-1 bg-slate-100"
+                    type="button"
+                    onClick={() => deleteClient(client.id)}
+                  >
+                    Deletar
+                  </button>
+                </li>
+              ))}
+        </ul>
+        <button
+          className="border rounded p-2 bg-slate-200 mt-4"
+          type="button"
+          onClick={getClients}
+        >
+          GET CLIENTS
+        </button>
+      </div>
+    </div>
+  );
+}
