4f5af3b56c5dcab585131b2e7bd2ec8a358410a1
Andre Rousselet <andre098@hotmail.com>
2022-08-19 14:31:14 -0300

    feat: import authentication service from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebaseConfig from "./firebase.config";
+import firebaseConfig from './firebase.config';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
+const auth = getAuth(app);
 // const analytics = getAnalytics(app);
 
-export {db};
+export { db, auth };
