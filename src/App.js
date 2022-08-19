b9455b8fc876cfc9079de2370afadbba3623496f
Andre Rousselet <andre098@hotmail.com>
2022-08-19 19:59:57 -0300

    feat: add CRUD functions to firebase as example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="container mx-auto font-light">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
-import Footer from '../components/Footer'
-import Header from '../components/Header'
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Clients from '../services/getClients';
 
 function Home() {
   return (
     <>
-      <Header pageTitle="HOME"/>
+      <Header pageTitle="HOME" />
+      <Clients />
       <Footer />
     </>
   );
